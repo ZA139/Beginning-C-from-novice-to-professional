bca68fae6b49826fd167f09538807284e54363dc
chhajed-ajay <chhajed7890@gmail.com>
2019-10-18 00:17:16 +0530

    I have removed the system(pause) thing to make code better

--- Untitled-1.c
+++ Untitled-1.c
@@ -1,31 +1,29 @@
-#include<stdio.h>
-#include<ctype.h>
+#include <stdio.h>
+#include <ctype.h>
 int main(void)
 {
-   char answer='N';
-   double total=0.0;
-   double value=0.0;
-   unsigned int count=0;
+  char answer = 'N';
+  double total = 0.0;
+  double value = 0.0;
+  unsigned int count = 0;
 
-   printf("\n This program calculates the average of "
-                                                      "any number");
-   for(; ; )
-   {
-       printf("\nEnter a value!");
-       scanf("%lf",&value);
-       total+=value;
-       ++count;
+  printf("\n This program calculates the average of "
+         "any number");
 
-       printf("Do you want to enter another value?(Y or N)");
-       getchar();
-       scanf("%c",&answer);
+  for (;;)
+  {
+    printf("\nEnter a value!");
+    scanf("%lf", &value);
+    total += value;
+    ++count;
      
-     if (tolower(answer)=='n')
-      break;
-     
-       
-   }
-   printf("\nThe average is %.2lf of all this number!",total/count);
-   system("pause");
-    return 0;
+    printf("Do you want to enter another value?(Y or N)");
+    getchar();
+    scanf("%c", &answer);
+
+    if (tolower(answer) == 'n') //to make it user friendly
+      break; 
+  }
+  printf("\nThe average of given numbers is %.2lf !", total / count);
+  return 0;
 }
